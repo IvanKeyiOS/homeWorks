82cb0d1ac0b1a966113f4eaa11b7f3cfa454fe93
IvanKeyiOS <ivanbeat888@gmail.com>
2024-11-05 20:19:55 +0100

    some change in hw 5 and 6

--- 06_ControlFlow.playground/Contents.swift
+++ 06_ControlFlow.playground/Contents.swift
@@ -23,18 +23,21 @@ repeat {
 print("=======================================")
 // - объявите строку произвольного значения и используя switch statement сделайте все гласные в строке заглавными. Распечатайте новое значение
 
-let string: String = "Hello there"
+var string: String = "watermelon"
 
-switch string {
-case "Swift":
-    print("SwIft")
-case "WOrld":
-    print("World")
-case "Hello there":
-    print("HEllO thErE")
-default:
-    print("ErrOr")
+for i in string {
+    switch i {
+    case "a":
+        string = string.replacingOccurrences(of: String("a"), with: "A")
+    case "e":
+        string = string.replacingOccurrences(of: String("e"), with: "E")
+    case "o":
+        string = string.replacingOccurrences(of: String("o"), with: "O")
+    default:
+        break
+    }
 }
+print(string)
 print("=======================================")
 // - объявите произвольное значение типа Int и напишите switch, который будет сравнивать это значение с несколькими range.
 
@@ -84,73 +87,46 @@ let devinGrade: [String : Double] = ["History": 4.4, "English": 4.6, "Geography"
 let kevinGrade: [String : Double] = ["History": 4.0, "English": 4.9, "Geography": 4.6, "Science": 4.3, "Maths": 4.4]
 let bradleyGrade: [String : Double] = ["History": 4.8, "English": 4.7, "Geography": 4.5, "Science": 4.8, "Maths": 4.9]
 
-for (subject, grade) in devinGrade {
-    print("Devin's \(subject) grade: \(grade)")
+var sumDevinGrade = 0.0
+var countDevinCourse = devinGrade.count
+for (key, value) in devinGrade {
+    sumDevinGrade += value
 }
-print("=======================================")
+let averageGradeDevin = sumDevinGrade / Double(countDevinCourse)
+print("Devin's average grade: \(averageGradeDevin)")
 
-for (subject, grade) in kevinGrade {
-    print("Kevin's \(subject) grade: \(grade)")
+var sumKevinGrade = 0.0
+var countKevinCourse = kevinGrade.count
+for (key, value) in kevinGrade {
+    sumKevinGrade += value
 }
-print("=======================================")
+let averageGradeKevin = sumKevinGrade / Double(countKevinCourse)
+print("Kevin's average grade: \(averageGradeKevin)")
 
-for (subject, grade) in bradleyGrade {
-    print("Bradley's \(subject) grade: \(grade)")
+var sumBradleyGrade = 0.0
+var countBradleyCourse = bradleyGrade.count
+for (key, value) in bradleyGrade {
+    sumBradleyGrade += value
 }
-
-print("=======================================")
-
-func averageGrade(a: Double, b: Double, c: Double, d: Double, e: Double) -> Double {
-    return (a + b + c + d + e) / 5
-}
-
-let devinScore: Double = averageGrade(a: devinGrade["History"] ?? 0.0, b: devinGrade["English"] ?? 0.0, c: devinGrade["Geography"] ?? 0.0, d: devinGrade["Science"] ?? 0.0, e: devinGrade["Maths"] ?? 0.0)
-print("Devin's average grade: \(devinScore)")
-
-let kevinScore: Double = averageGrade(a: kevinGrade["History"] ?? 0.0, b: kevinGrade["English"] ?? 0.0, c: kevinGrade["Geography"] ?? 0.0, d: kevinGrade["Science"] ?? 0.0, e: kevinGrade["Maths"] ?? 0.0)
-print("Kevin's average grade: \(kevinScore)")
-
-let bradleyScore: Double = averageGrade(a: bradleyGrade["History"] ?? 0.0, b: bradleyGrade["English"] ?? 0.0, c: bradleyGrade["Geography"] ?? 0.0, d: bradleyGrade["Science"] ?? 0.0, e: bradleyGrade["Maths"] ?? 0.0)
-print("Bradley's average grade: \(bradleyScore)")
+let averageGradeBradley = sumBradleyGrade / Double(countBradleyCourse)
+print("Bradley's average grade: \(averageGradeBradley)")
 
 print("=======================================")
 
-if devinScore > kevinScore && devinScore > bradleyScore && kevinScore > bradleyScore {
-    print("Devin \(devinScore)")
-    print("Kevin \(kevinScore)")
-    print("Bradley \(bradleyScore)")
-} else {
-    if kevinScore > devinScore && kevinScore > bradleyScore && devinScore > bradleyScore {
-        print("Kevin \(kevinScore)")
-        print("Devin \(devinScore)")
-        print("Bradley \(bradleyScore)")
-    } else {
-        if bradleyScore > kevinScore && bradleyScore > devinScore && kevinScore > devinScore {
-            print("Bradley \(bradleyScore)")
-            print("Kevin \(kevinScore)")
-            print("Devin \(devinScore)")
-        } else {
-            if devinScore > kevinScore && devinScore > bradleyScore && bradleyScore > kevinScore {
-                print("Devin \(devinScore)")
-                print("Bradley \(bradleyScore)")
-                print("Kevin \(kevinScore)")
-            } else {
-                if kevinScore > devinScore && kevinScore > bradleyScore && bradleyScore > devinScore {
-                    print("Kevin \(kevinScore)")
-                    print("Bradley \(bradleyScore)")
-                    print("Devin \(devinScore)")
-                } else {
-                    if bradleyScore > kevinScore && bradleyScore > devinScore && devinScore > kevinScore {
-                        print("Bradley \(bradleyScore)")
-                        print("Devin \(devinScore)")
-                        print("Kevin \(kevinScore)")
-                        
-                    }
-                }
-            }
-        }
-    }
+var averageStudentsGrade: [String: Double] = [:]
+averageStudentsGrade["Devin"] = averageGradeDevin
+averageStudentsGrade["Kevin"] = averageGradeKevin
+averageStudentsGrade["Bradley"] = averageGradeBradley
+    
+print(averageStudentsGrade)
+print("=======================================")
+
+for (key, value) in averageStudentsGrade {
+    if averageStudentsGrade.values.sorted() 
 }
+let sortedValues = averageStudentsGrade.values.sorted()
+print(sortedValues)
+   
 /*
  TODO: - ❓ К изучению:
  - Выучить синтаксис для всех statements and loops
