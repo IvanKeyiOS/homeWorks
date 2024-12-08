39efcb01f0dacb7044d4fb77fed634e1e1e5c414
IvanKeyiOS <ivanbeat888@gmail.com>
2024-12-08 23:28:16 +0100

    add changes in home work 16 version 3.0 and with uncompleted the last execise

--- 16_Generics.playground/Contents.swift
+++ 16_Generics.playground/Contents.swift
@@ -33,26 +33,22 @@ findMax(someElements: doubleArray)
 // Extra task*
 // Создайте generic протокол CompareProtocol, который объявляет функцию для сравнения двух элементов одного и того же типа. Метод должен возвращать Bool value как результат сравнения двух объектов. Объявите структуру или класс, подчините протоколу, добавьте реализацию. Создайте массив элементов и отсортируйте его, используя ваш метод.
 
-
+var doubleArrayForCompare = [1.9, 2.4, 2.5, 3.0, 3.3, 2.4, 1.6, 2.0, 5.0, 7.0, 6.3]
 
 protocol CompareProtocol {
-    func compareElements<SomeTypeForCompare: Comparable>(_ itemOne: SomeTypeForCompare, _ itemTwo: SomeTypeForCompare) -> Bool
+   func compareElements<SomeTypeForCompare: Comparable>(_ itemOne: SomeTypeForCompare, _ itemTwo: SomeTypeForCompare) -> Bool
 }
 
 class SomeTypeForCompare: CompareProtocol {
-    
-    func compareElements<SomeTypeForCompare: Comparable>(_ itemOne: SomeTypeForCompare, _ itemTwo: SomeTypeForCompare) -> Bool {
-        return itemOne < itemTwo
-        // Думаю что строка выше если и верно написана, то не до конца, еще чего-то не хватает, но к сожалению не знаю как это сделать верно :(
+   func compareElements<SomeTypeForCompare: Comparable>(_ itemOne: SomeTypeForCompare, _ itemTwo: SomeTypeForCompare) -> Bool {
+       return itemOne < itemTwo
+       // Думаю что строка выше если и верно написана, то не до конца, еще чего-то не хватает, но к сожалению не знаю как это сделать верно :(
     }
 }
 
 let someVariable = SomeTypeForCompare()
 
-var doubleArrayForCompare = [1.9, 2.4, 2.5, 3.0, 3.3, 2.4, 1.6, 2.0, 5.0, 7.0, 6.3]
-
 someVariable.compareElements(doubleArrayForCompare[2], doubleArrayForCompare[8])
-
 
 /*
  TODO: - ❓ Вопросы к изучению:
@@ -60,4 +56,3 @@ someVariable.compareElements(doubleArrayForCompare[2], doubleArrayForCompare[8])
  - Синтаксис объявления дженерик функции?
  - Как ограничить тип, который может быть использован в дженерик функции и дженерик типе?
  */
-
